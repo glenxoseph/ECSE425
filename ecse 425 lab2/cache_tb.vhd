79818b6f38ab4904cd479a72745348be9196d182
Glen <jiayuan.xu@mail.mcgill.ca>
2021-02-26 06:45:05 -0500

    testbench

--- ecse 425 lab2/cache_tb.vhd
+++ ecse 425 lab2/cache_tb.vhd
@@ -116,7 +116,204 @@ test_process : process
 begin
 
 -- put your tests here
-	
+
+-- Valid,   Dirty;  Read,   Tag Equal
+-- 0        0       0       0   A
+-- 0        0       0       1   IMPOSSIBLE
+-- 0        0       1       0   B1
+-- 0        0       1       1   IMPOSSIBLE
+-- 0        1       0       0   IMPOSSIBLE
+-- 0        1       0       1   IMPOSSIBLE
+-- 0        1       1       0   IMPOSSIBLE
+-- 0        1       1       1   IMPOSSIBLE
+-- 1        0       0       0   C2
+-- 1        0       0       1   B3
+-- 1        0       1       0   C1
+-- 1        0       1       1   B2
+-- 1        1       0       0   B5
+-- 1        1       0       1   B4
+-- 1        1       1       0   C3
+-- 1        1       1       1   A
+
+    reset <= '1';
+    s_read <='0';
+    s_write <= '0';
+    WAIT FOR clk_period;
+    reset <= '0';
+
+    WAIT FOR clk_period;
+
+    report "Test A";
+-- 0000, Invalid, Clean, Write, Miss
+-- brand new cache, everything is invalid/clean/miss
+
+    s_addr <= "11111111111111111111111000001111";
+    s_write <= '1';
+    s_writedata <= x"000F000A";
+    wait until rising_edge(s_waitrequest);
+    s_write <= '0';
+
+-- 1111, Valid, Dirty, Read, Hit
+-- this was written, so valid/dirty/hit
+    s_read <= '1';
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F000A" report "Test A FAILED" severity error;
+    s_read <= '0';
+
+    WAIT FOR clk_period;
+--helper
+    s_addr <= "01111111111111111111111000001111";
+    s_write <= '1';
+    s_writedata <= x"000F00AA";
+    wait until rising_edge(s_waitrequest);
+    s_write <= '0';
+-- x"000F000A" in memory
+    WAIT FOR clk_period;
+
+    reset <= '1';
+    s_read <='0';
+    s_write <= '0';
+    WAIT FOR clk_period;
+    reset <= '0';
+-- reset after this
+
+    report "Test B";
+-- 0010, Invalid, Clean, Read, Miss
+-- brand new cache, everything is invalid/clean/miss
+    s_read <= '1';
+    s_addr <= "11111111111111111111111000001111";
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F000A" report "Test B1 FAILED" severity error;
+    s_read <= '0';
+
+    WAIT FOR clk_period;
+
+-- 1011, Valid, Clean, Read, Hit
+-- already read, so valid/hit
+    s_read <= '1';
+    s_addr <= "11111111111111111111111000001111";
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F000A" report "Test B2 FAILED" severity error;
+    s_read <= '0';
+
+    WAIT FOR clk_period;
+
+-- 1001, Valid, Clean, Write, Hit
+-- this was read, so valid/hit
+
+    s_addr <= "11111111111111111111111000001111";
+    s_write <= '1';
+    s_writedata <= x"000F000F";
+    wait until rising_edge(s_waitrequest);
+    s_write <= '0';
+
+-- 1111, Valid, Dirty, Read, Hit
+-- this was written, so dirty
+
+    s_read <= '1';
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F000F" report "Test B3 FAILED" severity error;
+    s_read <= '0';
+
+    WAIT FOR clk_period;
+
+-- 1101, Valid, Dirty, Write, Hit
+-- this was written, so dirty
+
+    s_addr <= "11111111111111111111111000001111";
+    s_write <= '1';
+    s_writedata <= x"000F000E";
+    wait until rising_edge(s_waitrequest);
+    s_write <= '0';
+
+-- 1111, Valid, Dirty, Read, Hit
+
+    s_read <= '1';
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F000E" report "Test B4 FAILED" severity error;
+    s_read <= '0';
+
+    WAIT FOR clk_period;
+
+-- 1100, Valid, Dirty, Write, Miss
+-- this address is written, so valid and dirty, but the tag is missed
+
+    s_addr <= "11111111111110111111111000001111";
+    s_write <= '1';
+    s_writedata <= x"000F000B";
+    wait until rising_edge(s_waitrequest);
+    s_write <= '0';
+
+-- 1111, Valid, Dirty, Read, Hit
+
+    s_read <= '1';
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F000B" report "Test B5 FAILED" severity error;
+    s_read <= '0';
+
+    WAIT FOR clk_period;
+
+-- helper
+    s_addr <= "01111111111110111111111000001111";
+    s_write <= '1';
+    s_writedata <= x"000F00BB";
+    wait until rising_edge(s_waitrequest);
+    s_write <= '0';
+-- x"000F000B" should be in memory
+
+    reset <= '1';
+    s_read <='0';
+    s_write <= '0';
+    WAIT FOR clk_period;
+    reset <= '0';
+-- reset
+
+    WAIT FOR clk_period;
+
+    Report "Test C";
+
+-- 0010, Invalid, Clean, Read, Miss
+-- brand new cache, everything is invalid/clean/miss
+    s_read <= '1';
+    s_addr <= "11111111111110111111111000001111";
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F000B" report "Test C FAILED" severity error;
+    s_read <= '0';
+
+-- 1010, Valid, Clean, Read, Miss
+-- after first read, this address is valid, we use another tag so we miss
+    s_read <= '1';
+    s_addr <= "11111111111111111111111000001111";
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F000E" report "Test C1 FAILED" severity error;
+    s_read <= '0';
+
+-- 1000, Valid, Clean, Write, Miss
+-- after first read, this address is valid, we use another tag so we miss
+    s_addr <= "11111111111100111111111000001111";
+    s_write <= '1';
+    s_writedata <= x"000F00AA";
+    wait until rising_edge(s_waitrequest);
+    s_write <= '0';
+
+-- 1111, Valid, Dirty, Read, Hit
+    s_read <= '1';
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F00AA" report "Test C2 FAILED" severity error;
+    s_read <= '0';
+
+    WAIT FOR clk_period;
+
+-- 1110, Valid, Dirty, Read, Miss
+-- after first read, this address is valid, we use another tag so we miss
+    s_addr <= "11111111111111111111111000001111";
+    s_read <= '1';
+    wait until rising_edge(s_waitrequest);
+    assert s_readdata = x"000F000E" report "Test C3 FAILED" severity error;
+    s_read <= '0';
+
+    WAIT FOR clk_period;
+
 end process;
-	
+
 end;
