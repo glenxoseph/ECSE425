44bc2dab052d9ed1bd8cbd2c9e8175d750935710
Cheng Chen <cheng.chen2@mail.mcgill.ca>
2021-03-08 22:39:01 +0800

    create tcl file

--- ecse 425 lab2/cache.vhd
+++ ecse 425 lab2/cache.vhd
@@ -43,30 +43,24 @@ begin
 		variable counter : integer range 0 TO 5;
 		variable is_read : integer range 0 to 1;
 		begin
-			--report "begin process";
 			if reset = '1' then
 				state <= start;
-				--report "reset";
 				cache_struct <= (others=>(others=>'0'));
-				-- set to high by default
 				s_waitrequest <= '1';
 			elsif (rising_edge(clock) and clock = '1') then
 				offset := to_integer(unsigned(s_addr(3 downto 2))); -- ignore the last 2 bit of offset
 				idx := to_integer(unsigned(s_addr(8 downto 4))); -- 5 bit index
-				address <= cache_struct(idx)(133 downto 128)& s_addr (8 downto 0); 
-				
+				address <= cache_struct(idx)(133 downto 128)& s_addr (8 downto 0);
+
 			case state is
 
 				when start =>
-					--report "start";
 					s_waitrequest <= '1';
 					if s_write = '1' and s_read = '0' then
-						--report "state <= write_ready";
-						is_read := 0;					
+						is_read := 0;
 						state <= write_ready;
 					elsif s_read = '1' and s_write = '0' then
-						--report "state <= read_ready";
-						is_read := 1;	
+						is_read := 1;
 						state <= read_ready;
 					else
 						state <= start;
@@ -75,117 +69,84 @@ begin
 
 				when read_ready =>
 					if cache_struct(idx)(152) = '1' and s_addr(31 downto 9) = cache_struct(idx)(150 downto 128) then--read hit
-						--report "read hit, state <= read_state";
 						state <= read_state;
 					elsif (cache_struct(idx)(152) = '0' or s_addr(31 downto 9) /= cache_struct(idx)(150 downto 128)) and cache_struct(idx)(151) = '1' then -- read miss,dirty
-						--report "read miss,dirty ,state <= mem_write1;";
 						state <= mem_write1;
 					elsif (cache_struct(idx)(152) = '0' or s_addr(31 downto 9) /= cache_struct(idx)(150 downto 128)) and cache_struct(idx)(151) = '0' then -- read miss,clean
-						--report " read miss,clean ,state <= mem_read1;";
 						state <= mem_read1;
 					end if;
 				when write_ready =>
 					if cache_struct(idx)(152) = '1' and s_addr(31 downto 9) = cache_struct(idx)(150 downto 128) then --write hit
-						--report "write hit,state <= write_state" ;
 						state <= write_state;
 					elsif (cache_struct(idx)(152) = '0' or s_addr(31 downto 9) /= cache_struct(idx)(150 downto 128)) and cache_struct(idx)(151) = '1' then -- write miss,dirty
-						--report "write miss,dirty,state <= mem_write1";
 						state <= mem_write1;
 					elsif (cache_struct(idx)(152) = '0' or s_addr(31 downto 9) /= cache_struct(idx)(150 downto 128)) and cache_struct(idx)(151) = '0' then -- write miss,clean
-						 --report "write miss,clean, state <= mem_read1";
 						state <= mem_read1;
 					end if;
 
 				when read_state =>
-					--report "read state";
 					s_readdata <= cache_struct(idx)(127 downto 0) ((32*(offset + 1)) - 1 downto 32*offset);
-					--report "s_readdata="&integer'image(to_integer(unsigned(cache_struct(idx)(127 downto 0) ((32*(offset + 1)) - 1 downto 32*offset))));
 					s_waitrequest <= '0';
 					state <= start;
-					--report "finish read";
 				when write_state =>
-					--report "write state";
 					cache_struct(idx)(152) <= '1';
 					cache_struct(idx)(151) <= '1';
-					--report "s_addr="&integer'image(to_integer(unsigned(s_addr(14 downto 0))));
 					cache_struct(idx)(150 downto 128) <= s_addr(31 downto 9);
 					cache_struct(idx)(127 downto 0)((32*(offset + 1)) - 1 downto 32*offset) <= s_writedata;
-					--report "s_writedata="&integer'image(to_integer(unsigned(s_writedata)));
-					--report "data in cache after write="&integer'image(to_integer(unsigned(cache_struct(idx)(127 downto 0)((32*(offset + 1)) - 1 downto 32*offset))));
 					s_waitrequest <= '0';
 					state <= start;
 
 				when mem_write1 => --write data from cache to memory
-					--report "mem_write1";
 					if counter <= 3 and m_waitrequest = '1' then
-						--report "m_waitrequest = 1, counter < 3";
-						
-						--report "s_addr="&integer'image(to_integer(unsigned(s_addr(14 downto 0))));
-						--report "counter="&integer'image(counter);
-						--report "address1="&integer'image(to_integer(unsigned(cache_struct(idx)(133 downto 128))));
-						--report "address2="&integer'image(to_integer(unsigned(s_addr (8 downto 0))));
-						if counter = 0 then 
+						if counter = 0 then
 							m_addr <= to_integer(unsigned(address));
-						else 
+						else
 							m_addr <= to_integer(unsigned(address)) + counter;
-						end if;						
+						end if;
 						m_write <= '1';
 						m_read <= '0';
 						m_writedata <= cache_struct(idx)(127 downto 0) ((8*counter + 32*offset + 7) downto  (8*counter + 32*offset));
-						--report "m_addr="&integer'image(to_integer(unsigned(address)) + counter);
-						--report "m_writedata=" & integer'image(to_integer(unsigned(cache_struct(idx)(127 downto 0) ((8*counter + 32*offset + 7) downto  (8*counter + 32*offset)))));
 						counter := counter + 1;
 						state <= mem_write1;
 					elsif counter = 4 then
-						--report "mem_write1,counter = 4";
 						counter := 0;
 						m_write <= '0';
 						state <= mem_read1;
-					else	
-						--report "mem_write1,else";
+					else
 						m_write <= '0';
 						state <= mem_write1;
 					end if;
 
 				when mem_read1 => --read data from memory to cache
-					--report "mem_read1";
 					if m_waitrequest = '1' then
-						--report "mem_read1,m_waitrequest = '1'";
 						m_read <= '1';
 						m_write <= '0';
-						--report "s_addr="&integer'image(to_integer(unsigned(s_addr(14 downto 0))));
 						m_addr <= to_integer(unsigned(s_addr(14 downto 0)))+ counter ;
-						--report "m_addr="&integer'image(to_integer(unsigned(s_addr(14 downto 0))) + counter);
 						state <= read_memwait;
 					else
 						state <= mem_read1;
 					end if;
 
-				when read_memwait => 
-					--report "wait mem read";
-					
+				when read_memwait =>
+
 					if m_waitrequest = '0' and counter <= 3 then
-						--report "wait mem read,m_waitrequest = '0'";
 						cache_struct(idx)(127 downto 0) ((8*counter +32*offset + 7) downto (8*counter + 32*offset)) <= m_readdata;
-						--report "m_readdata="&integer'image(to_integer(unsigned(m_readdata)));
 						counter := counter + 1;
 						m_read <= '0';
 						state <= mem_read1;
 					elsif counter = 4 then
-						--report "wait mem read,counter = 4";
 						counter := 0;
 						cache_struct(idx)(152) <= '1';
 						cache_struct(idx)(151) <= '0';
-						--report "s_addr="&integer'image(to_integer(unsigned(s_addr(14 downto 0))));
 						cache_struct(idx)(150 downto 128) <= s_addr(31 downto 9);
 						m_read <= '0';
 						m_write <= '0';
-						if is_read = 1 then 
+						if is_read = 1 then
 							state <= read_state;
-						elsif is_read = 0 then 
+						elsif is_read = 0 then
 							state <= write_state;
 						end if;
-					else 
+					else
 						state <= read_memwait;
 					end if;
 				end case;
@@ -195,6 +156,5 @@ begin
 
 
 
--- make circuits here
 
 end arch;
